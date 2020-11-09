9223d7cb0998095091e1284d2eb94dd9af39b764
Lukas Borawski <lukas.borawski@gmail.com>
2020-11-10 00:12:42 +0100

    Update note.ts

--- store/note.ts
+++ store/note.ts
@@ -1,46 +1,44 @@
 import { Module, ActionTree, GetterTree, MutationTree } from 'vuex'
-import { MainState, NoteState } from '~/types/store'
+import { MainState, NotesState } from '~/types/store'
 import { Note } from '~/types'
 
-export const state: NoteState = {
-  note: {},
+export const state: NotesState = {
   notes: [],
 }
 
-const mutations: MutationTree<NoteState> = {
+const mutations: MutationTree<NotesState> = {
   setNotes(state, payload: Note[]) {
     state.notes = payload
   },
 }
-const actions: ActionTree<NoteState, MainState> = {
+const actions: ActionTree<NotesState, MainState> = {
   async saveNote({ state, commit }, payload: Note) {
     const { id } = payload
-    // JSON stringify add later/ask how to pass object
     await (this as any).$localForage.setItem(id, JSON.stringify(payload))
-    commit('setNotes', [...state.notes, payload])
+    commit('setNotes', [...state.notes, payload] as Note[])
   },
-  async removeNote({ state, commit }, id: string) {
-    // suggest to filter array of notes
-    await (this as any).$localForage.removeItem(id)
+  async removeNote({ state, commit }, $id: string) {
+    await $localForage.removeItem($id)
+    comit('setNotes', state.notes.filter((note: Note) => note.id !== $id) as Note[])
   },
   async getNotes({ state, commit }) {
-    commit('setNotes', []) // add later as a bug of empty array
+    commit('setNotes', [])
     if ((this as any).$localForage) {
       const keys = await (this as any).$localForage.keys()
       keys.map(async (key: string) => {
         const note = await (this as any).$localForage.getItem(key)
-        commit('setNotes', [...state.notes, JSON.parse(note)])
+        commit('setNotes', [...state.notes, JSON.parse(note)] as Note[])
       })
     }
   },
 }
-const getters: GetterTree<NoteState, MainState> = {
+const getters: GetterTree<NotesState, MainState> = {
   notes: ({ notes }) => notes,
 }
 
 const namespaced: boolean = true
 
-export const note: Module<NoteState, MainState> = {
+export const note: Module<NotesState, MainState> = {
   namespaced,
   state,
   mutations,
